192f99b38b9e8e240f2933f07a29306ca013260f
Robson Oliveira <oliveira.robs@outlook.com>
2023-09-26 10:30:23 -0700

    rota corrigida

--- BlogDev/react-blog/src/components/ArticleCard/ArticleCard.js
+++ BlogDev/react-blog/src/components/ArticleCard/ArticleCard.js
@@ -19,6 +19,7 @@ function ArticleCard() {
         fetch('https://vidaembits-001-site1.ftempurl.com/Posts') // Ajuste essa URL conforme necessário
             .then(response => response.json())
             .then(data => {
+                console.log(data);
                 setPosts(data);
                 setLoading(false);
             })
@@ -39,14 +40,14 @@ function ArticleCard() {
             <div ref={topRef} className='artigos-list' >
                 {articlesToShow.map(post =>
                 (
-                    <Link to={`/article`} className="article-link">
+                    <Link to={`/article/${post.postID}`} className="article-link">
                         <div className="artigo-card" key={post.postID}>
                             <div className="artigo-image-overlay"></div>
                             <img src='data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18a3eb13545%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18a3eb13545%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2299.12312316894531%22%20y%3D%2296.3%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E' alt={post.titulo} className="artigo-image" />
                             <div className="artigo-content">
                                 <h3 className="artigo-title">{post.titulo}</h3>
                                 <div className="artigo-category">{post.categoria}</div>
-                                <Link to={`/article/${post.postID}`} className="artigo-author">
+                                <Link  className="artigo-author">
                                     <i className="icon-user"></i>
                                     {post.titulo}
                                 </Link>
@@ -69,7 +70,7 @@ function ArticleCard() {
         </div>
 
     );
-   
+
 }
 
 export default ArticleCard;
